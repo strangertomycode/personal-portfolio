c571c4100a578c535fdd88f662345cf1ec373b80
Sreeram P <psreeram3015@proton.me>
2026-05-30 18:01:38 +0530

    Set header z index to 50

--- src/loayouts/Navbar.jsx
+++ src/loayouts/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
   return (
-    <header className="fixed top-0 left-0 right-0 bg-transparent py-5">
+    <header className="fixed top-0 left-0 right-0 bg-transparent py-5 z-50">
       <nav className="container mx-auto px-6 flex items-center justify-between">
         <a href="#" className="text-xl font-bold tracking-tight hover:text-primary">
           SP <span className="text-primary">.</span>
