2bb1c316f33992f1886dd62529331cfeabfb5d4d
Sreeram P <psreeram3015@proton.me>
2026-05-30 19:27:29 +0530

    Handle Navbar behaviour when scrolled

--- src/loayouts/Navbar.jsx
+++ src/loayouts/Navbar.jsx
@@ -1,6 +1,6 @@
 import { Menu, X } from "lucide-react";
 import { Button } from "../components";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const navLinks = [
   { href: "#about", label: "About" },
@@ -11,9 +11,24 @@ const navLinks = [
 
 const Navbar = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [isScrolled, setisScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setisScrolled(window.scrollY > 50);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
-    <header className="fixed top-0 left-0 right-0 bg-transparent py-5 z-50">
+    <header
+      className={`fixed top-0 left-0 right-0 transition-all duration-500 ${
+        isScrolled ? "glass-strong py-3" : "bg-transparent py-5"
+      }  z-50`}
+    >
       <nav className="container mx-auto px-6 flex items-center justify-between">
         <a href="#" className="text-xl font-bold tracking-tight hover:text-primary">
           SP <span className="text-primary">.</span>
