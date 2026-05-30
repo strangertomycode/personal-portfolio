4e195bdcd52e62ee07d0c98d07d411b5a86072be
Sreeram P <psreeram3015@proton.me>
2026-05-30 19:05:02 +0530

    Remove skill section as it will be in Hero section

--- src/sections/Skill.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Skill = () => {
-  return <section>Skill</section>;
-};
-
-export default Skill;
--- src/sections/index.js
+++ src/sections/index.js
@@ -3,6 +3,5 @@ import Contact from "./Contact";
 import Education from "./Education";
 import Hero from "./Hero";
 import Project from "./Project";
-import Skill from "./Skill";
 
-export { About, Contact, Education, Hero, Project, Skill };
+export { About, Contact, Education, Hero, Project };
